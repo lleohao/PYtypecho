8762c2647af45e4d781ece7acaf481d81ed006b8
lleohao <lleohao@hotmail.com>
2015-11-28 12:37:33 +0800

    添加模板文件

--- app/ui/views.py
+++ app/ui/views.py
@@ -2,16 +2,8 @@ from flask import render_template, redirect, flash, request, url_for
 from flask.ext.login import login_required
 from . import ui
 from ..modules import Post
-from .forms import userForm
 
 
 @ui.route("/")
 def index():
-    posts = Post.objects()
-    return render_template("/ui/index.html", posts=posts )
-
-
-@ui.route("/test")
-def test():
-    form = userForm()
-    return render_template("/ui/test.html", form=form)
+    return "ok"
