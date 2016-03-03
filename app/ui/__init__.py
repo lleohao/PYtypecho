ba60270539e45e4e6e528b0facd62449cb9c9448
lleohao <lleohao@hotmail.com>
2016-03-03 15:27:06 +0800

    调整前端需求文件

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-ui = Blueprint("ui", __name__, template_folder="templates", static_folder="static")
+ui = Blueprint("ui", __name__, template_folder="templates")
 
 from . import views
