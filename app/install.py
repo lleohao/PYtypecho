ce9e0dad94ff3e9e14c8f588cc5df732b8384b45
lleohao <lleohao@hotmail.com>
2016-03-09 22:45:18 +0800

    增强系统稳定性

--- app/install.py
+++ app/install.py
@@ -6,4 +6,4 @@ def install():
     User(username="admin", password="admin", email="admin@admin.com", url="admin.admin", screenName="admin",
          group="administrator").save()
     Category(name="默认分类", slug="normal", description="这是系统默认的分类").save()
-    Options(url="http://test_url.url", title="test_blog", keyword="blog,python,mongodb").save()
+    Options(url="http://test_url.url", title="test_blog", keyword="blog,python,mongodb", description="test_blog_description").save()
