e54c4b5df20d5ff1602d14922dbe7ad1fe63b1bf
lleohao <lleohao@hotmail.com>
2016-08-07 09:18:58 +0800

    添加静态文件

---
+++ app/admin/static/js/custom.js
@@ -0,0 +1,34 @@
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        initFunction: function () {
+            /*MENU 
+             ------------------------------------*/
+            $('#main-menu').metisMenu();
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+                }
+            });
+        }
+    };
+    $(document).ready(function () {
+        mainApp.initFunction();
+        $("#sideNav").click(function () {
+            if ($(this).hasClass('closed')) {
+                $('.navbar-side').animate({left: '0px'});
+                $(this).removeClass('closed');
+                $('#page-wrapper').animate({'margin-left': '260px'});
+
+            }
+            else {
+                $(this).addClass('closed');
+                $('.navbar-side').animate({left: '-260px'});
+                $('#page-wrapper').animate({'margin-left': '0px'});
+            }
+        });
+    });
+}(jQuery));
